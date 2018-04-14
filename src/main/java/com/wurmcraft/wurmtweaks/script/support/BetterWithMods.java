913ffc1fdc216b03af3d2dcff408bbb9d7e0fd44
Wurmatron <wurmatron@gmail.com>
2018-04-14 02:37:32 -0400

    Updated BWM

--- src/main/java/com/wurmcraft/wurmtweaks/script/support/BetterWithMods.java
+++ src/main/java/com/wurmcraft/wurmtweaks/script/support/BetterWithMods.java
@@ -1,13 +1,16 @@
 package com.wurmcraft.wurmtweaks.script.support;
 
+
+import betterwithmods.common.BWRegistry;
 import betterwithmods.common.blocks.mechanical.tile.TileEntityFilteredHopper;
 import betterwithmods.common.registry.HopperInteractions;
 import betterwithmods.common.registry.anvil.AnvilCraftingManager;
 import betterwithmods.common.registry.anvil.ShapedAnvilRecipe;
-import betterwithmods.common.registry.blockmeta.managers.SawManager;
-import betterwithmods.common.registry.bulk.manager.*;
+import betterwithmods.common.registry.bulk.manager.MillManager;
 import betterwithmods.common.registry.heat.BWMHeatRegistry;
+import betterwithmods.module.gameplay.SawRecipes;
 import betterwithmods.util.InvUtils;
+import cofh.thermalexpansion.util.managers.machine.CrucibleManager;
 import com.google.common.base.Preconditions;
 import com.wurmcraft.wurmtweaks.api.ScriptFunction;
 import com.wurmcraft.wurmtweaks.common.ConfigHandler;
@@ -36,16 +39,13 @@ public class BetterWithMods extends ModSupport {
 	@Override
 	public void init () {
 		if (ConfigHandler.removeAllMachineRecipes) {
-			SawManager.WOOD_SAW.getRecipes ().clear ();
-			SawManager.STEEL_SAW.getRecipes ().clear ();
-			CauldronManager.getInstance ().getRecipes ().clear ();
-			StokedCauldronManager.getInstance ().getRecipes ().clear ();
-			CrucibleManager.getInstance ().getRecipes ().clear ();
-			StokedCrucibleManager.getInstance ().getRecipes ().clear ();
+			BWRegistry.WOOD_SAW.getRecipes ().clear ();
+			BWRegistry.CAULDRON.getRecipes ().clear ();
+			BWRegistry.CRUCIBLE.getRecipes ().clear ();
 			HopperInteractions.RECIPES.clear ();
 			AnvilCraftingManager.ANVIL_CRAFTING.clear ();
 			AnvilCraftingManager.RECIPE_CACHE.clear ();
-			MillManager.getInstance ().getRecipes ().clear ();
+			BWRegistry.MILLSTONE.getRecipes ().clear ();
 		}
 	}
 
@@ -58,7 +58,7 @@ public class BetterWithMods extends ModSupport {
 			isValid (input[index]);
 			inputStacks.add (convertS (input[index]));
 		}
-		CauldronManager.getInstance ().addRecipe (convertS (input[0]),inputStacks.toArray (new ItemStack[0]));
+		BWRegistry.CAULDRON.addUnstokedRecipe (convertS (input[0]),inputStacks.toArray (new ItemStack[0]));
 	}
 
 	@ScriptFunction
@@ -70,7 +70,7 @@ public class BetterWithMods extends ModSupport {
 			isValid (input[index]);
 			inputStacks.add (convertS (input[index]));
 		}
-		StokedCauldronManager.getInstance ().addRecipe (convertS (input[0]),inputStacks.toArray (new ItemStack[0]));
+		BWRegistry.CAULDRON.addStokedRecipe (convertS (input[0]),inputStacks.toArray (new ItemStack[0]));
 	}
 
 	@ScriptFunction
@@ -82,7 +82,7 @@ public class BetterWithMods extends ModSupport {
 			isValid (input[index]);
 			inputStacks.add (convertS (input[index]));
 		}
-		CrucibleManager.getInstance ().addRecipe (convertS (input[0]),inputStacks.toArray (new ItemStack[0]));
+		BWRegistry.CRUCIBLE.addUnstokedRecipe (convertS (input[0]),inputStacks.toArray (new ItemStack[0]));
 	}
 
 	@ScriptFunction
@@ -94,7 +94,7 @@ public class BetterWithMods extends ModSupport {
 			isValid (input[index]);
 			inputStacks.add (convertS (input[index]));
 		}
-		StokedCrucibleManager.getInstance ().addRecipe (convertS (input[0]),inputStacks.toArray (new ItemStack[0]));
+		BWRegistry.CRUCIBLE.addStokedRecipe (convertS (input[0]),inputStacks.toArray (new ItemStack[0]));
 	}
 
 	@ScriptFunction
@@ -105,43 +105,14 @@ public class BetterWithMods extends ModSupport {
 			isValid (input[index]);
 			inputStacks.add (convertS (input[index]));
 		}
-		MillManager.getInstance ().addRecipe (0,convertS (input[0]),inputStacks.toArray (new ItemStack[0]));
-
+		BWRegistry.MILLSTONE.addMillRecipe (convertS (input[0]),inputStacks);
 	}
 
 	@ScriptFunction
 	public void addWoodSaw (String line) {
 		String[] input = verify (line,line.split (" ").length == 2,"addWoodSaw('<output> <input>')");
 		isValid (input[0],input[1]);
-		SawManager.WOOD_SAW.addRecipe (convertS (input[1]),convertS (input[0]));
-	}
-
-	@ScriptFunction
-	public void addSteelSaw (String line) {
-		String[] input = verify (line,line.split (" ").length == 2,"addSteelSaw('<output> <input>')");
-		isValid (input[0],input[1]);
-		SawManager.STEEL_SAW.addRecipe (convertS (input[1]),convertS (input[0]));
-	}
-
-	@ScriptFunction
-	public void addFilteredHopper (String line) {
-		String[] input = verify (line,line.split (" ").length == 3,"addFilteredHopper('<output> <input> <type>')");
-		isValid (input[0],input[1]);
-		isValid (EnumInputType.INTEGER,input[2]);
-		HopperInteractions.addHopperRecipe (new HopperInteractions.HopperRecipe (convertNI (input[2]),convertS (input[1]),convertS (input[0])) {
-			@Override
-			public void craft (EntityItem inputStack,World world,BlockPos pos) {
-				InvUtils.ejectStackWithOffset (world,inputStack.getPosition (),output.copy ());
-				TileEntityFilteredHopper tile = (TileEntityFilteredHopper) world.getTileEntity (pos);
-				assert tile != null;
-				ItemStackHandler inventory = tile.inventory;
-				ItemStack sand = secondaryOutput.get (world.rand.nextInt (secondaryOutput.size ())).copy ();
-				if (!InvUtils.insert (inventory,sand,false).isEmpty ()) {
-					InvUtils.ejectStackWithOffset (world,inputStack.getPosition (),sand);
-				}
-				onCraft (world,pos,inputStack);
-			}
-		});
+		BWRegistry.WOOD_SAW.addRecipe (convertS (input[1]),convertS (input[0]));
 	}
 
 	@ScriptFunction
@@ -160,6 +131,6 @@ public class BetterWithMods extends ModSupport {
 		isValid (input[0]);
 		isValid (EnumInputType.INTEGER,input[1]);
 		Block heatBlock = Block.getBlockFromItem (convertS (input[0]).getItem ());
-		BWMHeatRegistry.addHeatSource (heatBlock,convertS (input[0]).getItemDamage (),convertNI (input[1]));
+		BWMHeatRegistry.addHeatSource (heatBlock,convertNI (input[1]));
 	}
 }
