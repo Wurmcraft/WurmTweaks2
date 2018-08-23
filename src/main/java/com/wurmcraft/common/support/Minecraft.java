eeffacd6fd819ba1c86e1ef93570da5e5c9c6fa5
Wurmatron <wurmatron@gmail.com>
2018-08-22 20:42:40 -0400

    Added Furnace and Brewing Functions

--- src/main/java/com/wurmcraft/common/support/Minecraft.java
+++ src/main/java/com/wurmcraft/common/support/Minecraft.java
@@ -11,12 +11,18 @@ import com.wurmcraft.common.script.ScriptExecutor;
 import com.wurmcraft.common.support.utils.Converter;
 import com.wurmcraft.common.support.utils.InvalidRecipe;
 import com.wurmcraft.common.support.utils.RecipeUtils;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import joptsimple.internal.Strings;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.common.brewing.BrewingOreRecipe;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.registries.ForgeRegistry;
@@ -25,20 +31,99 @@ import org.cliffc.high_scale_lib.NonBlockingHashSet;
 @Support(modid = "minecraft", threaded = true)
 public class Minecraft {
 
-  public static NonBlockingHashSet<IRecipe> scriptRecipes = new NonBlockingHashSet<>();
-  public static NonBlockingHashSet<FurnaceRecipe> scriptFurnace = new NonBlockingHashSet<>();
-  public static NonBlockingHashSet<OreEntry> scriptOreEntry = new NonBlockingHashSet<>();
+  public static NonBlockingHashSet<IRecipe> scriptRecipes;
+  public static NonBlockingHashSet<FurnaceRecipe> scriptFurnace;
+  public static NonBlockingHashSet<OreEntry> scriptOreEntry;
+  public static NonBlockingHashSet<BrewingOreRecipe> scriptBrewing;
 
   @InitSupport
   public void init() {
+    scriptRecipes = new NonBlockingHashSet<>();
+    scriptFurnace = new NonBlockingHashSet<>();
+    scriptOreEntry = new NonBlockingHashSet<>();
+    scriptBrewing = new NonBlockingHashSet<>();
     if (ScriptExecutor.reload) {
       removeRecipes();
       scriptRecipes.clear();
       scriptOreEntry.clear();
       WurmTweaks.logger
           .warn("OreDictionary entries have not been reloaded. (Old ones will still exist)");
+      // TODO Remove Furnace, OreDict, Brewing Recipes on Reload
       scriptFurnace.clear();
+      scriptBrewing.clear();
     }
+  }
+
+  @FinalizeSupport()
+  public void finalizeSupport() {
+    recipeLock(false);
+    if (ConfigHandler.removeAllRecipes) {
+      removeRecipes();
+    }
+    ForgeRegistries.RECIPES.registerAll(scriptRecipes.toArray(new IRecipe[0]));
+    scriptOreEntry.forEach(entry -> OreDictionary.registerOre(entry.values, entry.entry));
+    for (FurnaceRecipe recipe : scriptFurnace) {
+      for (ItemStack input : recipe.input.getMatchingStacks()) {
+        FurnaceRecipes.instance().addSmeltingRecipe(input, recipe.output, recipe.exp);
+      }
+    }
+    for (BrewingOreRecipe brewingOreRecipe : scriptBrewing) {
+      BrewingRecipeRegistry.addRecipe(brewingOreRecipe);
+    }
+    recipeLock(true);
+  }
+
+  private void recipeLock(boolean lock) {
+    ForgeRegistry recipes = (ForgeRegistry) ForgeRegistries.RECIPES;
+    if (lock) {
+      recipes.freeze();
+    } else {
+      recipes.unfreeze();
+    }
+  }
+
+  @ScriptFunction(modid = "minecraft", inputFormat = "ItemStack ItemStack/OreDictionary ...")
+  public void addShapeless(Converter converter, String[] line) {
+    Object[] shapelessInputs = RecipeUtils
+        .getShapelessItems(Arrays.copyOfRange(line, 1, line.length), converter);
+    scriptRecipes.add(RecipeUtils
+        .createShapelessRecipe((ItemStack) converter.convert(line[0], 1), shapelessInputs));
+  }
+
+  @ScriptFunction(modid = "minecraft", inputFormat = "ItemStack ***")
+  public void addShaped(Converter converter, String[] line) {
+    if (line.length >= 2) {
+      scriptRecipes.add(RecipeUtils.createShapedRecipe((ItemStack) converter.convert(line[0], 1),
+          RecipeUtils.getShapedRecipe(Arrays.copyOfRange(line, 1, line.length)).toArray()));
+    } else {
+      WurmTweaks.logger.error("Invalid Shaped Format '" + Strings.join(line, " ") + "'");
+    }
+  }
+
+  @ScriptFunction(modid = "minecraft", precedence = true, inputFormat = "ItemStack String ...")
+  public void addOreEntry(Converter converter, String[] input) {
+    for (int index = 1; index < input.length; index++) {
+      scriptOreEntry.add(new OreEntry((ItemStack) converter.convert(input[0], 0), input[index]));
+    }
+  }
+
+  @ScriptFunction(modid = "minecraft", inputFormat = "ItemStack ItemStack/OreDictionary")
+  public void addFurnace(Converter converter, String[] input) {
+    scriptFurnace.add(new FurnaceRecipe((ItemStack) converter.convert(input[0], 0),
+        (Ingredient) converter.convert(input[1], 1)));
+  }
+
+  @ScriptFunction(modid = "minecraft", inputFormat = "ItemStack ItemStack ItemStack")
+  public void addBrewing(Converter converter, String[] input) {
+    scriptBrewing.add(new BrewingOreRecipe((ItemStack) converter.convert(input[0], 1),
+        createList((ItemStack) converter.convert(input[2])),
+        (ItemStack) converter.convert(input[1])));
+  }
+
+  private static List<ItemStack> createList(ItemStack... stack) {
+    List<ItemStack> list = new ArrayList<>();
+    Collections.addAll(list, stack);
+    return list;
   }
 
   private void removeRecipes() {
@@ -60,52 +145,6 @@ public class Minecraft {
     }
     return true;
   }
-
-  @FinalizeSupport()
-  public void finalizeSupport() {
-    recipeLock(false);
-    if (ConfigHandler.removeAllRecipes) {
-      removeRecipes();
-    }
-    ForgeRegistries.RECIPES.registerAll(scriptRecipes.toArray(new IRecipe[0]));
-    scriptOreEntry.forEach(entry -> OreDictionary.registerOre(entry.values, entry.entry));
-    recipeLock(true);
-  }
-
-  private void recipeLock(boolean lock) {
-    ForgeRegistry recipes = (ForgeRegistry) ForgeRegistries.RECIPES;
-    if (lock) {
-      recipes.freeze();
-    } else {
-      recipes.unfreeze();
-    }
-  }
-
-  @ScriptFunction(modid = "minecraft", inputFormat = "ItemStack ItemStack/OreDictionary ...")
-  public void addShapeless(Converter converter, String[] line) {
-    Object[] shapelessInputs = RecipeUtils
-        .getShapelessItems(Arrays.copyOfRange(line, 1, line.length), converter);
-    scriptRecipes.add(RecipeUtils
-        .createShapelessRecipe((ItemStack) converter.convert(line[0], 1), shapelessInputs));
-  }
-
-  @ScriptFunction(modid = "minecraft")
-  public void addShaped(Converter converter, String[] line) {
-    if (line.length >= 2) {
-      scriptRecipes.add(RecipeUtils.createShapedRecipe((ItemStack) converter.convert(line[0], 1),
-          RecipeUtils.getShapedRecipe(Arrays.copyOfRange(line, 1, line.length)).toArray()));
-    } else {
-      WurmTweaks.logger.error("Invalid Shaped Format '" + Strings.join(line, " ") + "'");
-    }
-  }
-
-  @ScriptFunction(modid = "minecraft", precedence = true)
-  public void addOreEntry(Converter converter, String[] input) {
-    for (int index = 1; index < input.length; index++) {
-      scriptOreEntry.add(new OreEntry((ItemStack) converter.convert(input[0], 0), input[index]));
-    }
-  }
-
 
   public class FurnaceRecipe {
 
