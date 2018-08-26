8b7e0e722aa932e23704c758fe88ee2932a23e25
Wurmatron <wurmatron@gmail.com>
2018-08-26 13:42:54 -0400

    Added Industrial Forgoing, PnumaticCraft

--- src/main/java/com/wurmcraft/common/support/IndustrialForgoing.java
+++ src/main/java/com/wurmcraft/common/support/IndustrialForgoing.java
@@ -1,5 +1,94 @@
 package com.wurmcraft.common.support;
 
+import static com.buuz135.industrial.api.IndustrialForegoingHelper.addBioReactorEntry;
+import static com.buuz135.industrial.api.IndustrialForegoingHelper.addLaserDrillEntry;
+import static com.buuz135.industrial.api.IndustrialForegoingHelper.addProteinReactorEntry;
+import static com.buuz135.industrial.api.IndustrialForegoingHelper.addSludgeRefinerEntry;
+
+import com.buuz135.industrial.api.recipe.BioReactorEntry;
+import com.buuz135.industrial.api.recipe.LaserDrillEntry;
+import com.buuz135.industrial.api.recipe.ProteinReactorEntry;
+import com.buuz135.industrial.api.recipe.SludgeEntry;
+import com.wurmcraft.api.script.anotations.FinalizeSupport;
+import com.wurmcraft.api.script.anotations.InitSupport;
+import com.wurmcraft.api.script.anotations.ScriptFunction;
+import com.wurmcraft.api.script.anotations.ScriptFunction.FunctionType;
+import com.wurmcraft.api.script.anotations.Support;
+import com.wurmcraft.common.ConfigHandler;
+import com.wurmcraft.common.script.ScriptExecutor;
+import com.wurmcraft.common.support.utils.Converter;
+import net.minecraft.item.ItemStack;
+import org.cliffc.high_scale_lib.NonBlockingHashSet;
+
+@Support(modid = "industrialforegoing")
 public class IndustrialForgoing {
 
+  private NonBlockingHashSet<BioReactorEntry> bioReactor;
+  private NonBlockingHashSet<SludgeEntry> sludge;
+  private NonBlockingHashSet<ProteinReactorEntry> protein;
+  private NonBlockingHashSet<LaserDrillEntry> laser;
+
+  @InitSupport
+  public void init() {
+    if (bioReactor == null) {
+      bioReactor = new NonBlockingHashSet<>();
+      sludge = new NonBlockingHashSet<>();
+      protein = new NonBlockingHashSet<>();
+      laser = new NonBlockingHashSet<>();
+    }
+    if (ConfigHandler.removeAllMachineRecipes) {
+      BioReactorEntry.BIO_REACTOR_ENTRIES.clear();
+      SludgeEntry.SLUDGE_RECIPES.clear();
+      ProteinReactorEntry.PROTEIN_REACTOR_ENTRIES.clear();
+      LaserDrillEntry.LASER_DRILL_ENTRIES.clear();
+    } else if (ScriptExecutor.reload) {
+      bioReactor.forEach(entry -> BioReactorEntry.BIO_REACTOR_ENTRIES.remove(entry));
+      bioReactor.clear();
+      sludge.forEach(entry -> SludgeEntry.SLUDGE_RECIPES.remove(entry));
+      sludge.clear();
+      protein.forEach(entry -> ProteinReactorEntry.PROTEIN_REACTOR_ENTRIES.remove(entry));
+      protein.clear();
+      laser.forEach(entry -> LaserDrillEntry.LASER_DRILL_ENTRIES.remove(entry));
+      laser.clear();
+    }
+  }
+
+  @FinalizeSupport
+  public void finishSupport() {
+    for (BioReactorEntry recipe : bioReactor) {
+      addBioReactorEntry(recipe);
+    }
+    for (SludgeEntry recipe : sludge) {
+      addSludgeRefinerEntry(recipe);
+    }
+    for (ProteinReactorEntry recipe : protein) {
+      addProteinReactorEntry(recipe);
+    }
+    for (LaserDrillEntry recipe : laser) {
+      addLaserDrillEntry(recipe);
+    }
+  }
+
+  @ScriptFunction(modid = "industrialforegoing", inputFormat = "ItemStack")
+  public void addBioReactor(Converter converter, String[] line) {
+    bioReactor.add(new BioReactorEntry((ItemStack) converter.convert(line[0], 1)));
+  }
+
+  @ScriptFunction(modid = "industrialforegoing", inputFormat = "ItemStack Integer")
+  public void addSludgeRefiner(Converter converter, String[] line) {
+    sludge
+        .add(new SludgeEntry((ItemStack) converter.convert(line[0], 1), Integer.parseInt(line[1])));
+  }
+
+  @ScriptFunction(modid = "industrialforegoing", inputFormat = "ItemStack")
+  public void addProteinReactor(Converter converter, String[] line) {
+    protein.add(new ProteinReactorEntry((ItemStack) converter.convert(line[0], 1)));
+  }
+
+  @ScriptFunction(modid = "industrialforegoing", inputFormat = "ItemStack Integer Integer", typeData = "Laser", type = FunctionType.Linked)
+  public void addLaser(Converter converter, String[] line) {
+    laser.add(
+        new LaserDrillEntry(Integer.parseInt(line[1]), (ItemStack) converter.convert(line[0], 1),
+            Integer.parseInt(line[2])));
+  }
 }
--- src/main/java/com/wurmcraft/common/support/PnumaticCraft.java
+++ src/main/java/com/wurmcraft/common/support/PnumaticCraft.java
@@ -1,5 +1,104 @@
 package com.wurmcraft.common.support;
 
+import com.wurmcraft.api.script.anotations.FinalizeSupport;
+import com.wurmcraft.api.script.anotations.InitSupport;
+import com.wurmcraft.api.script.anotations.ScriptFunction;
+import com.wurmcraft.api.script.anotations.Support;
+import com.wurmcraft.common.ConfigHandler;
+import com.wurmcraft.common.script.ScriptExecutor;
+import com.wurmcraft.common.support.utils.Converter;
+import java.util.Arrays;
+import me.desht.pneumaticcraft.api.PneumaticRegistry;
+import me.desht.pneumaticcraft.common.recipes.AmadronOfferManager;
+import me.desht.pneumaticcraft.common.recipes.AssemblyRecipe;
+import me.desht.pneumaticcraft.common.recipes.PressureChamberRecipe;
+import net.minecraft.item.ItemStack;
+import org.cliffc.high_scale_lib.NonBlockingHashSet;
+
+@Support(modid = "pneumaticcraft")
 public class PnumaticCraft {
 
+  private NonBlockingHashSet<Object[]> drill;
+  private NonBlockingHashSet<Object[]> laser;
+  private NonBlockingHashSet<Object[]> pressure;
+  private NonBlockingHashSet<Object[]> amadron;
+  private NonBlockingHashSet<Object[]> defaultAmadron;
+
+
+  @InitSupport
+  public void init() {
+    drill = new NonBlockingHashSet<>();
+    laser = new NonBlockingHashSet<>();
+    pressure = new NonBlockingHashSet<>();
+    amadron = new NonBlockingHashSet<>();
+    defaultAmadron = new NonBlockingHashSet<>();
+    if (ConfigHandler.removeAllMachineRecipes) {
+      AssemblyRecipe.drillRecipes.clear();
+      AssemblyRecipe.laserRecipes.clear();
+      PressureChamberRecipe.chamberRecipes.clear();
+      AmadronOfferManager.getInstance().getStaticOffers().clear();
+      AmadronOfferManager.getInstance().getPeriodicOffers().clear();
+      AmadronOfferManager.getInstance().getAllOffers().clear();
+    } else if (ScriptExecutor.reload) {
+      // TODO Remove Recipes On Reload
+      drill.clear();
+      laser.clear();
+      pressure.clear();
+      amadron.clear();
+      defaultAmadron.clear();
+    }
+  }
+
+  @FinalizeSupport
+  public void finishSupport() {
+    for (Object[] recipe : drill) {
+      PneumaticRegistry.getInstance().getRecipeRegistry()
+          .addAssemblyDrillRecipe(recipe[0], recipe[1]);
+    }
+    for (Object[] recipe : laser) {
+      PneumaticRegistry.getInstance().getRecipeRegistry()
+          .addAssemblyLaserRecipe(recipe[0], recipe[1]);
+    }
+    for (Object[] recipe : pressure) {
+      PneumaticRegistry.getInstance().getRecipeRegistry()
+          .registerPressureChamberRecipe((ItemStack[]) recipe[0], (float) recipe[1],
+              (ItemStack[]) recipe[2]);
+    }
+    for (Object[] recipe : defaultAmadron) {
+      PneumaticRegistry.getInstance().getRecipeRegistry()
+          .registerDefaultStaticAmadronOffer(recipe[0], recipe[1]);
+    }
+    for (Object[] recipe : amadron) {
+      PneumaticRegistry.getInstance().getRecipeRegistry()
+          .registerDefaultPeriodicAmadronOffer(recipe[0], recipe[1]);
+    }
+  }
+
+  @ScriptFunction(modid = "pneumaticcraft", inputFormat = "ItemStack ItemStack")
+  public void addAssemblyDrill(Converter converter, String[] line) {
+    drill.add(new Object[]{converter.convert(line[1]), converter.convert(line[0])});
+  }
+
+  @ScriptFunction(modid = "pneumaticcraft", inputFormat = "ItemStack ItemStack")
+  public void addAssemblyLaser(Converter converter, String[] line) {
+    laser.add(new Object[]{converter.convert(line[1]), converter.convert(line[0])});
+  }
+
+  @ScriptFunction(modid = "pneumaticcraft", inputFormat = "ItemStack Integer ItemStack ...")
+  public void addPressureChamber(Converter converter, String[] line) {
+    pressure.add(new Object[]{
+        converter.getBulkItemsAsList(Arrays.copyOfRange(line, 2, line.length)).toArray(
+            new ItemStack[0]), Float.parseFloat(line[1]),
+        new ItemStack[]{(ItemStack) converter.convert(line[0])}});
+  }
+
+  @ScriptFunction(modid = "pneumaticcraft", inputFormat = "ItemStack ItemStack")
+  public void addDefaultAmadron(Converter converter, String[] line) {
+    defaultAmadron.add(new Object[]{converter.convert(line[0]), converter.convert(line[1])});
+  }
+
+  @ScriptFunction(modid = "pneumaticcraft", inputFormat = "ItemStack ItemStack")
+  public void addAmadron(Converter converter, String[] line) {
+    amadron.add(new Object[]{converter.convert(line[0]), converter.convert(line[1])});
+  }
 }
